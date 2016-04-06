3d60de7a3ca8dd7389f84a2afd5ed0b40cfa0876
Azman Zakaria <azman120474@gmail.com>
2016-04-06 14:15:58 +0800

    test commit

---
+++ views/pemohon/permohonan/borang2.php
@@ -0,0 +1,166 @@
+<div>
+    <div class="alert alert-danger">
+        <b>ARAHAN</b>
+        <ul>
+            <li>Lengkapkan borang dengan betul</li>
+            <li>Maklumat yang lengkap sahaja akan diproses</li>
+            <li>Permohonan yang baru dihantar akan membatalkan permohonan terdahulu</li>
+        </ul>
+    </div>
+    <div>
+        <legend>BUTIRAN PERIBADI</legend>
+        <table class="table">
+            <tr>
+                <td width="20%">1. Kad Pengenalan</td>
+                <td>: <input type="text" value="740412095063" class="disabled"></td>
+            </tr>
+            <tr>
+                <td>2. Nama Penuh Pemohon</td>
+                <td>: <input type="text" value="Azman bin Zakaria" class="disabled"></td>
+            </tr>
+            <tr>
+                <td>3. Jantina</td>
+                <td>: 
+                    <input type="radio" checked="" name="jantina"> Lelaki
+                    <input type="radio" name="jantina"> Perempuan
+                </td>
+            </tr>
+            <tr>
+                <td>4. No Tel Bimbit</td>
+                <td>: <input type="text"> contoh : 012-2220000</td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <legend>BUTIRAN CALON ORANG KURANG UPAYA (OKU) / BEKEPERLUAN KHAS</legend>
+        <table class="table">
+            <tr>
+                <td width="20%">5. No Pendaftaran OKU</td>
+                <td>: <input type="text"></td>
+            </tr>
+            <tr>
+                <td>6. Kategori OKU</td>
+                <td>: 
+                    <input type="checkbox"> Rabun
+                    <input type="checkbox"> Buta
+                    <input type="checkbox"> Bisu
+                    <input type="checkbox"> Pekak
+                    <br>
+                    <input type="checkbox"> Cacat anggota; sila nyatakan : <input type="text">
+                    <br>
+                    <input type="checkbox"> Lain-Lain; sila nyatakan : <input type="text">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    7. Keperluan Semasa Peperiksaan
+                    * Tertakluk kepada kesediaan di pusat peperiksaan berkenaan
+                </td>
+                <td>: 
+                    <input type="checkbox"> Tambahan Masa
+                    <input type="checkbox"> Cetakan Besar
+                    <input type="checkbox"> Braille
+                    <input type="checkbox"> Pembaca
+                    <input type="checkbox"> Jurutulis
+                    <input type="checkbox"> Pembantu Khas
+                    <input type="checkbox"> Laluan Khas Kerusi Roda
+                    <br>
+                    <input type="checkbox"> Lain-Lain; sila nyatakan : <input type="text">
+                </td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <legend>BUTIRAN PERKHIDMATAN</legend>
+        <table class="table">
+            <tr>
+                <td>8. Skim Perkhidmatan</td>
+                <td><input type="text" value="Pembantu Tadbir (Pekeranian/operasi)" class="disabled"></td>
+            </tr>
+            <tr>
+                <td>9. Gred Jawatan</td>
+                <td><input type="text" value="N17" class="disabled"></td>
+            </tr>
+            <tr>
+                <td>10. Tarikh lantikan (jawatan semasa)</td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td>11. Tarikh Sah Perkhidmatan</td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td>
+                    12. Tarikh memangku <br>
+                    * calon memohon ujian kemahiran ICT yang sedang memangku perlu lengkapkan
+                    tarikh memangku
+                </td>
+                <td><input type="text" value=""></td>
+            </tr>
+            <tr>
+                <td>13. Taraf Perkhidmatan</td>
+                <td>
+                    <select>
+                        <option>--PILIH--</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>14. Taraf Pelantikan</td>
+                <td>
+                    <select>
+                        <option>--PILIH--</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>15. Gelaran Ketua Pejabat</td>
+                <td><input type="text"> Contoh : Ketua Pengarah</td>
+            </tr>
+            <tr>
+                <td>16. Jabatan</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>17. Alamat Pejabat</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>18. Poskod</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>19. Negeri</td>
+                <td>
+                    <select>
+                        <option>--PILIH--</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>20. Bandar</td>
+                <td>
+                    <select>
+                        <option>--PILIH--</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>21. No. tel Pejabat</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>22. Emel</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>23. Emel ketua jabatan / bahagian</td>
+                <td><input type="text"></td>
+            </tr>
+        </table>
+    </div>
+    <div>
+        <legend>BUTIRAN PEPERIKSAAN</legend>
+    </div>
+</div>
+<div></div>
