775bdb4a7b80dd2091fddc4b68e5a6988cd553a7
Azman Zakaria <azman120474@gmail.com>
2016-04-06 20:47:59 +0800

    test commit 3

--- models/Dummy.php
+++ models/Dummy.php
@@ -2,5 +2,7 @@
 namespace app\models;
 // model ini point / connect table dummy dlm db spj
 class Dummy extends \yii\db\ActiveRecord {
-    
+    public function tableName() {
+        return 'dummy';
+    }
 }
